5d95d352d0ce7332133b57b9d510bd43ea73bfd5
nazarmafia <42717068+nazarmafia@users.noreply.github.com>
2018-09-01 00:16:56 +0500

    Add files via upload

---
+++ php/sm.php
@@ -0,0 +1,33 @@
+<?php
+
+include 'config.php';
+
+$email = $mail;
+$subject = 'Новый заявка: {NAME} {TEL}';
+$message = '<html>
+<body>
+    <p><b>Новая заявка:</b></p>	
+    <p><b>Имя</b> {NAME}</p>
+    <p><b>Телефон:</b> {TEL}</p>
+    </body>
+</html>';
+
+if(isset($_POST['tel'])){
+
+    $headers = "From: " . $email . "\r\n";
+//$headers .= "Reply-To: ". strip_tags($_POST['req-email']) . "\r\n";
+//$headers .= "CC: susan@example.com\r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+    
+    foreach($_POST as $key=>$value)
+        $subject=str_replace('{'.strtoupper($key).'}', htmlspecialchars($value), $subject);
+
+    foreach($_POST as $key=>$value)
+        $message=str_replace('{'.strtoupper($key).'}', htmlspecialchars($value), $message);
+
+    echo
+        mail($email, $subject, $message, $headers)?'true':'false';
+
+}
+
